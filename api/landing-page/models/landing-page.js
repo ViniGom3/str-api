fc9df9cdce8cd15fe555145b1cdf0a7a2e45c7cd
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-05-16 23:44:35 -0300

    refact: remove unnecessary await

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -12,10 +12,11 @@ const netlifyWebhook = strapi.config.get("custom.netlifyWebhook")
 module.exports = {
   lifecycles: {
     async afterCreate(_, __) {
-      netlifyWebhook && await axios.post(netlifyWebhook)
+      netlifyWebhook && axios.post(netlifyWebhook)
     },
     async afterUpdate(_, __, ___) {
-      netlifyWebhook && await axios.post(netlifyWebhook)
+      netlifyWebhook && axios.post(netlifyWebhook)
     },
   },
 };
+
